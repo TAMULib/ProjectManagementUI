abecd2c3568cffb1df086914c2ac22a23c067030
William Welling <wwelling@library.tamu.edu>
2018-03-22 11:39:17 -0500

    updated htaccess and sample app config

--- app/config/appConfig_sample.js
+++ app/config/appConfig_sample.js
@@ -1,38 +1,38 @@
-var appConfig = { 
+var appConfig = {
 
-		'version': '1.0.0',
+    'version': '1.0.0',
 
-		'allowAnonymous': true,
+    'allowAnonymous': true,
 
-		'anonymousRole': 'ROLE_ANONYMOUS',
+    'anonymousRole': 'ROLE_ANONYMOUS',
 
-		// Set this to the webService if mocking AuthService
-		
-		'authService': '',
-		'webService': '', 
-		
-		'storageType': 'session',
+    // Set this to the webService if mocking AuthService
 
-		'logging': {
-			'log': true,
-			'info': true,
-			'warn': true,
-			'error': true,
-			'debug': true
-		},
-		
-		'stompDebug': false,
+    'authService': 'https://labs.library.tamu.edu/auth3',
+    'webService': 'http://localhost:9001',
 
-		/*
-		Determines the type of connection stomp will attempt to make with the service.
-		TYPES:  websocket, xhr-streaming, xdr-streaming, eventsource, iframe-eventsource, 
-				htmlfile, iframe-htmlfile, xhr-polling, xdr-polling, iframe-xhr-polling,
-				jsonp-polling
-		*/
-		'sockJsConnectionType': ['websocket', 'xhr-streaming', 'xhr-polling', 'xdr-streaming', 'xdr-polling', 'iframe-eventsource',  'iframe-htmlfile', 'jsonp-polling'],
-		
-		// Set this to 'admin' or 'user' if using mock AuthService
-		// otherwise set to null or false
-		
-		'mockRole': null
+    'storageType': 'session',
+
+    'logging': {
+        'log': true,
+        'info': true,
+        'warn': true,
+        'error': true,
+        'debug': true
+    },
+
+    'stompDebug': false,
+
+    /*
+    Determines the type of connection stomp will attempt to make with the service.
+    TYPES:  websocket, xhr-streaming, xdr-streaming, eventsource, iframe-eventsource, 
+        htmlfile, iframe-htmlfile, xhr-polling, xdr-polling, iframe-xhr-polling,
+        jsonp-polling
+    */
+    'sockJsConnectionType': ['websocket', 'xhr-streaming', 'xhr-polling', 'xdr-streaming', 'xdr-polling', 'iframe-eventsource', 'iframe-htmlfile', 'jsonp-polling'],
+
+    // Set this to 'admin' or 'user' if using mock AuthService
+    // otherwise set to null or false
+
+    'mockRole': null
 };
