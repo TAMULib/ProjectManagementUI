b8a3c275531a1153acdc7b86c4960933a7bf991d
William Welling <wwelling@library.tamu.edu>
2018-08-16 13:09:57 -0500

    services tests

--- tests/mocks/services/mockActiveSprintsService.js
+++ tests/mocks/services/mockActiveSprintsService.js
@@ -1,11 +1,8 @@
-angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', function ($q) {
-
-  this.getActiveSprints = function () {
-    return [{
-      "id": "7820",
-      "name": "Sprint 3",
-      "project": "chronam",
-      "cards": [{
+var mockActiveSprints = [{
+    "id": "7820",
+    "name": "Sprint 3",
+    "project": "chronam",
+    "cards": [{
         "id": "7802",
         "number": "B-03351",
         "type": "Feature",
@@ -13,7 +10,7 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "As a Customer I want to see 8 random issues on the front page, so that blank entries don't ever show",
         "estimate": 2,
         "assignees": []
-      }, {
+    }, {
         "id": "7803",
         "number": "D-01153",
         "type": "Defect",
@@ -21,7 +18,7 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "Date limiting functionallity in search not working.",
         "estimate": 3,
         "assignees": []
-      }, {
+    }, {
         "id": "7804",
         "number": "B-03352",
         "type": "Feature",
@@ -29,7 +26,7 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "Remove route for for viewing MARC records.",
         "estimate": 0,
         "assignees": []
-      }, {
+    }, {
         "id": "7805",
         "number": "B-03353",
         "type": "Feature",
@@ -37,7 +34,7 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "Add pagination to bottom of search results.",
         "estimate": 0.5,
         "assignees": []
-      }, {
+    }, {
         "id": "7806",
         "number": "B-03354",
         "type": "Feature",
@@ -45,21 +42,21 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "Add metadata to search results",
         "estimate": 3,
         "assignees": []
-      }, {
+    }, {
         "id": "7854",
         "number": "B-03367",
         "type": "Feature",
         "status": "Done",
         "name": "Rearrange and remove uneeded fields from advanced search page.",
         "assignees": []
-      }, {
+    }, {
         "id": "7856",
         "number": "B-03369",
         "type": "Feature",
         "status": "Accepted",
         "name": "Search bar should redirect to search results when submitted.",
         "assignees": []
-      }, {
+    }, {
         "id": "7859",
         "number": "D-01155",
         "type": "Defect",
@@ -67,25 +64,30 @@ angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', 
         "name": "Batch loading is failing on call to library of congress' website.",
         "description": "<p>Attempting to retrieve an xml file from the LC website, but being denied permission.</p>\n<p>Moved a copy of the file to:</p>\n<p>http://php.library.tamu.edu/marc.xml</p>\n<p>And now hitting that url for the file.</p>",
         "assignees": [{
-          "id": "6616",
-          "name": "Ryan Laddusaw",
-          "avatar": "no_avatar.png"
+            "id": "6616",
+            "name": "Ryan Laddusaw",
+            "avatar": "no_avatar.png"
         }]
-      }, {
+    }, {
         "id": "7860",
         "number": "B-03370",
         "type": "Feature",
         "status": "Accepted",
         "name": "Add advanced search link.",
         "assignees": [{
-          "id": "6616",
-          "name": "Ryan Laddusaw",
-          "avatar": "no_avatar.png"
+            "id": "6616",
+            "name": "Ryan Laddusaw",
+            "avatar": "no_avatar.png"
         }]
-      }]
-    }];
-  };
+    }]
+}];
 
-  this.updated = $q.defer().promise;
+angular.module('mock.activeSprintsService', []).service('ActiveSprintsService', function ($q) {
+
+    this.getActiveSprints = function () {
+        return mockActiveSprints;
+    };
+
+    this.updated = $q.defer().promise;
 
 });
--- tests/unit/services/projectsStatsServiceTest.js
+++ tests/unit/services/projectsStatsServiceTest.js
@@ -7,7 +7,8 @@ describe('service: ProjectsStatsService', function () {
         module('app');
         module('mock.wsApi');
         module('mock.projectRepo');
-        inject(function (_$q_, _WsApi_, _ProjectRepo_, _ProjectsStatsService_) {
+        inject(function (_$rootScope_, _$q_, _WsApi_, _ProjectRepo_, _ProjectsStatsService_) {
+            $rootScope = _$rootScope_;
             $q = _$q_;
             WsApi = _WsApi_;
             ProjectRepo = _ProjectRepo_;
@@ -18,6 +19,51 @@ describe('service: ProjectsStatsService', function () {
     describe('Is the service defined', function () {
         it('should be defined', function () {
             expect(ProjectsStatsService).toBeDefined();
+        });
+    });
+
+    describe('Are the service methods defined', function () {
+        it('refreshProjectsStats should be defined', function () {
+            expect(ProjectsStatsService.refreshProjectsStats).toBeDefined();
+            expect(typeof ProjectsStatsService.refreshProjectsStats).toEqual('function');
+        });
+        it('getProjectsStats should be defined', function () {
+            expect(ProjectsStatsService.getProjectsStats).toBeDefined();
+            expect(typeof ProjectsStatsService.getProjectsStats).toEqual('function');
+        });
+        it('getById should be defined', function () {
+            expect(ProjectsStatsService.getById).toBeDefined();
+            expect(typeof ProjectsStatsService.getById).toEqual('function');
+        });
+    });
+
+    describe('Are the service properties defined', function () {
+        it('ready should be defined', function () {
+            expect(ProjectsStatsService.ready).toBeDefined();
+            expect(typeof ProjectsStatsService.ready).toEqual('object');
+        });
+    });
+
+    describe('Do the service methods work as expected', function () {
+        it('refreshProjectsStats should fetch project stats', function () {
+            deferred = $q.defer();
+            spyOn(WsApi, 'fetch').and.returnValue(deferred.promise);
+            ProjectsStatsService.refreshProjectsStats();
+            deferred.resolve(mockProjectsStats);
+            expect(WsApi.fetch).toHaveBeenCalledWith(apiMapping.ProjectsStats.all);
+        });
+        it('getProjectsStats should get project stats', function () {
+            spyOn(ProjectRepo, 'reset');
+            $rootScope.$apply();
+            var projectsStats = ProjectsStatsService.getProjectsStats();
+            expect(projectsStats).toEqual(mockProjectsStats);
+            expect(ProjectRepo.reset).toHaveBeenCalled();
+        });
+        it('getById should get project stats by id', function () {
+            $rootScope.$apply();
+            ProjectsStatsService.getById(1).then(function (projectStats) {
+                expect(projectStats).toEqual(mockProjectsStats[0]);
+            });
         });
     });
 
