6702b477d2f85b6efb1596779c30c9550cba6404
Kevin Day <kday@library.tamu.edu>
2019-12-17 10:09:30 -0600

    Update directive tests

--- tests/unit/directives/customHeaderDirectiveTest.js
+++ tests/unit/directives/customHeaderDirectiveTest.js
@@ -1,15 +1,40 @@
 describe("directive: customHeader", function () {
+  var $compile, $q, $scope, directive, element;
 
-  var scope, element;
-
-  beforeEach(function () {
-    module("core");
-    module("app");
-    module("views/directives/header.html");
-    inject(function ($rootScope, _$compile_) {
-      scope = $rootScope.$new();
+  var initializeVariables = function() {
+    inject(function (_$q_, _$compile_) {
+      $q = _$q_;
       $compile = _$compile_;
     });
+  };
+
+  var initializeDirective = function(settings) {
+    inject(function (_$rootScope_) {
+      $scope = _$rootScope_.$new();
+
+      var attr = settings && settings.attr ? settings.attr : "";
+      var body = settings && settings.body ? settings.body : "";
+
+      element = angular.element("<custom-header " + attr + ">" + body + "</custom-header>");
+      directive = $compile(element)($scope);
+
+      $scope.$digest();
+    });
+  };
+
+  beforeEach(function() {
+    module("core");
+    module("app");
+    module("templates");
+
+    installPromiseMatchers();
+    initializeVariables();
   });
 
+  describe("Is the directive defined", function () {
+    it("should be defined", function () {
+      initializeDirective();
+      expect(directive).toBeDefined();
+    });
+  });
 });
--- tests/unit/directives/remoteProjectManagerFormDirectiveTest.js
+++ tests/unit/directives/remoteProjectManagerFormDirectiveTest.js
@@ -1,20 +1,17 @@
 describe("directive: remoteProjectManagerForm", function () {
+  var $compile, $q, $scope, directive, element, formSettings, formModel;
 
-  var scope, element;
-
-  beforeEach(function () {
-    module("core");
-    module("app");
-    module("views/directives/remoteProjectManagerForm.html");
-    inject(function ($rootScope, _$compile_) {
-      scope = $rootScope.$new();
+  var initializeVariables = function() {
+    inject(function (_$q_, _$compile_) {
+      $q = _$q_;
       $compile = _$compile_;
 
-      scope.model = {
+      formModel = {
         name: "",
         type: ""
       };
-      scope.settings = [{
+
+      formSettings = [{
         "type": "text",
         "key": "url",
         "gloss": "URL",
@@ -35,9 +32,39 @@ describe("directive: remoteProjectManagerForm", function () {
         "gloss": "Token",
         "visible": false
       }];
-      element = $compile("<remote-project-manager-form management-settings='settings' model='model'></remote-project-manager-form>")(scope);
-      scope.$apply();
     });
+  };
+
+  var initializeDirective = function(settings) {
+    inject(function (_$rootScope_) {
+      $scope = _$rootScope_.$new();
+
+      var attr = settings && settings.attr ? settings.attr : "management-settings='settings' model='model'></remote-project-manager-form";
+      var body = settings && settings.body ? settings.body : "";
+
+      element = angular.element("<remote-project-manager-form " + attr + ">" + body + "</remote-project-manager-form>");
+      directive = $compile(element)($scope);
+
+      $scope.model = formModel;
+      $scope.settings = formSettings;
+
+      $scope.$digest();
+    });
+  };
+
+  beforeEach(function() {
+    module("core");
+    module("app");
+    module("templates");
+
+    installPromiseMatchers();
+    initializeVariables();
   });
 
+  describe("Is the directive defined", function () {
+    it("should be defined", function () {
+      initializeDirective();
+      expect(directive).toBeDefined();
+    });
+  });
 });
