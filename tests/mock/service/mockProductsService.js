e235239fdaf409edd18a0ee50bb1b697da8f3510
Kevin Day <kday@library.tamu.edu>
2020-05-08 18:30:08 -0500

    Implement fetching on change and dimplify internal request service design
    I accidentally left an incomplete @todo that was intended to be implemented in the relevant card.
    Simplify the implementation and update the tests while I am at it.

--- tests/mock/service/mockProductsService.js
+++ tests/mock/service/mockProductsService.js
@@ -1,25 +1,49 @@
-var mockProductsService = function ($q, ProductRepo, WsApi) {
+var mockProductsService = function ($q, $timeout) {
   var service = mockService($q);
 
   var products = {};
+  var remoteProducts = {};
 
   service.getById = function (id) {
-    // @todo
-    return {};
+    return $q(function (resolve, reject) {
+      for (var i in dataProductRepo1) {
+        if (dataProductRepo1[i].id === scopeId) {
+          resolve(dataProductRepo1[i]);
+        }
+      }
+      reject(undefined);
+    });
   }
-
-  service.getProductRemoteProducts = function (id) {
-    // @todo
-    return {};
-  };
 
   service.getProducts = function () {
     return products;
   };
 
-  service.refreshProducts = function () {
-    // @todo
+  service.getRemoteProductInfo = function (productId) {
+    for (var i in products) {
+      if (products[i].id == productId) {
+        return products[i];
+      }
+    }
   };
+
+  service.getRemoteProducts = function () {
+    return remoteProducts;
+  };
+
+  service.refreshProducts = function () {
+    return messagePromise();
+  };
+
+  service.refreshRemoteProducts = function (productId) {
+    if (dataFeatureRequest1.id == productId) {
+      return notifyPromise($timeout, $q.defer(), dataFeatureRequest1);
+    }
+
+    return rejectPromise($q.defer());
+  };
+
+  service.ready = $q.defer().promise;
 
   return service;
 };
