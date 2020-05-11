e3afe7b31808f12dd3793857e78ab80cf287ace4
Kevin Day <kday@library.tamu.edu>
2020-05-11 11:13:49 -0500

    Add guard to filter

--- app/filters/availableRemoteProductFilter.js
+++ app/filters/availableRemoteProductFilter.js
@@ -1,5 +1,6 @@
 app.filter('availableRemoteProduct', function() {
   return function(input, usedRPs) {
+    if (!input) return false;
     return input.filter(function(val) {
       if (usedRPs) {
         return !usedRPs.some(function(rp) {
