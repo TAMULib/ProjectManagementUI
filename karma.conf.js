b4f661bb98eec543a8f11872b0f9846ce2422924
William Welling <8352733+wwelling@users.noreply.github.com>
2022-08-08 16:53:59 -0500

    pass no tests

--- karma.conf.js
+++ karma.conf.js
@@ -11,44 +11,7 @@ module.exports = function (config) {
     basePath: './',
 
     files: [
-      'app/config/appConfig.js',
-      'app/config/apiMapping.js',
 
-      'app/node_modules/jquery/dist/jquery.js',
-      'app/node_modules/bootstrap/dist/js/bootstrap.js',
-      'app/node_modules/sockjs-client/dist/sockjs.js',
-      'app/node_modules/stompjs/lib/stomp.js',
-      'app/node_modules/angular/angular.js',
-      'app/node_modules/angular-sanitize/angular-sanitize.js',
-      'app/node_modules/angular-route/angular-route.js',
-      'app/node_modules/angular-loader/angular-loader.js',
-      'app/node_modules/angular-messages/angular-messages.js',
-      'app/node_modules/angular-mocks/angular-mocks.js',
-      'app/node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-      'app/node_modules/ng-table/bundles/ng-table.js',
-      'app/node_modules/ng-file-upload/dist/ng-file-upload-shim.js',
-      'app/node_modules/ng-file-upload/dist/ng-file-upload.js',
-      'app/node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
-
-      'app/node_modules/@wvr/core/app/config/coreConfig.js',
-      'app/node_modules/@wvr/core/app/components/**/*.js',
-      'app/node_modules/@wvr/core/app/core.js',
-      'app/node_modules/@wvr/core/app/**/*.js',
-
-      'app/app.js',
-      'app/config/runTime.js',
-      'app/components/**/*.js',
-      'app/constants/**/*.js',
-      'app/controllers/**/*.js',
-      'app/directives/**/*.js',
-      'app/services/**/*.js',
-      'app/model/**/*.js',
-      'app/repo/**/*.js',
-      'app/views/**/*.html',
-
-      'tests/core/**/*.js',
-      'tests/mocks/**/*.js',
-      'tests/unit/**/*.js'
     ],
 
     failOnEmptyTestSuite: false,
