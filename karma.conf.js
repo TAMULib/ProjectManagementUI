8ce640ea85eb87a8311295db377986ba1b2399c7
Kevin Day <kday@library.tamu.edu>
2022-08-09 10:53:18 -0500

    Fix karma test configuration.

--- karma.conf.js
+++ karma.conf.js
@@ -6,12 +6,49 @@ module.exports = function (config) {
       'app/**/*.html': ['ng-html2js']
     },
 
-    reporters: ['progress', 'coverage'],
+    reporters: ['progress', 'coverage', 'coveralls'],
 
     basePath: './',
 
     files: [
+      'dist/appConfig.js',
+      'app/config/apiMapping.js',
 
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/bootstrap/dist/js/bootstrap.js',
+      'node_modules/sockjs-client/dist/sockjs.js',
+      'node_modules/stompjs/lib/stomp.js',
+      'node_modules/angular/angular.js',
+      'node_modules/angular-sanitize/angular-sanitize.js',
+      'node_modules/angular-route/angular-route.js',
+      'node_modules/angular-loader/angular-loader.js',
+      'node_modules/angular-messages/angular-messages.js',
+      'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
+      'node_modules/ng-table/bundles/ng-table.js',
+      'node_modules/ng-file-upload/dist/ng-file-upload-shim.js',
+      'node_modules/ng-file-upload/dist/ng-file-upload.js',
+      'node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js',
+
+      'node_modules/@wvr/core/app/config/coreConfig.js',
+      'node_modules/@wvr/core/app/components/**/*.js',
+      'node_modules/@wvr/core/app/core.js',
+      'node_modules/@wvr/core/app/**/*.js',
+
+      "tests/testSetup.js",
+
+      'app/app.js',
+      'app/config/runTime.js',
+      'app/controllers/**/*.js',
+      'app/directives/**/*.js',
+      'app/services/**/*.js',
+      'app/model/**/*.js',
+      'app/repo/**/*.js',
+      'app/**/*.html',
+
+      'tests/core/**/*.js',
+      'tests/mock/**/*.js',
+      'tests/unit/**/*.js'
     ],
 
     failOnEmptyTestSuite: false,
@@ -20,18 +57,18 @@ module.exports = function (config) {
 
     frameworks: ['jasmine'],
 
-    browsers: ['Firefox', 'Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
 
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        base: 'ChromeHeadless'
       }
     },
 
     plugins: [
       'karma-chrome-launcher',
       'karma-coverage',
+      'karma-coveralls',
       'karma-firefox-launcher',
       'karma-jasmine',
       'karma-junit-reporter',
