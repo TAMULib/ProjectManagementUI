6653fd269a57d10202575d95aa5fd0f28e9bf2ff
William Welling <8352733+wwelling@users.noreply.github.com>
2022-09-20 08:55:15 -0500

    Weaver upgrade (#128)
    * dev deeployment updates
    
    * update default auth service
    
    * add engines to package.json

--- .wvr/build-config.js
+++ .wvr/build-config.js
@@ -15,6 +15,12 @@ const config = {
     module: false
   },
 
+  stats: {},
+
+  client: {
+    overlay: false,
+  },
+
   copy: [
     {
       from: './node_modules/bootstrap/fonts',
@@ -39,7 +45,7 @@ const config = {
         return content
           .toString()
           .replace('${STOMP_DEBUG}', 'false')
-          .replace('${AUTH_SERVICE_URL}', 'https://labs.library.tamu.edu/authfix')
+          .replace('${AUTH_SERVICE_URL}', 'https://labs.library.tamu.edu/auth/2x')
           .replace('${WEB_SERVICE_URL}', 'http://localhost:9001/products');
       },
     },
